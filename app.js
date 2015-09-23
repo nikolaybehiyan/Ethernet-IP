4df20a7d2bb4bd90f14f45d5c1b06cc07453b067
nikolay_behiyan <nikolay.behiyan@gmail.com>
2015-09-23 17:44:27 +0300

    Upload Project. This project describes Ethernet/IP protocol. Its used for Adam 6151EI Data Acquisition module. This project has also integrated  opc UA server

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var conn = new AdamEIP;
 var opcServer = new opcUAServer();
 var working = 0;
 conn.initiateConnection({port: 44818, host: '192.168.202.200'}, connected);
-
 function connected(err) {
   if (typeof(err) !== "undefined") {
     // We have an error.  Maybe the PLC is not reachable.
@@ -30,5 +29,8 @@ function connected(err) {
   }
 }
 
+//forever start -o out.log -e err.log --spinSleepTime 5000 -c node app.js
+//forever stopall
+//forever stop app.js
 
 
